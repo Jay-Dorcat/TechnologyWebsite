39277127c31473c283752de18871acbaff48c0ed
Dorcat <jaysharp.lee@gmail.com>
2026-06-24 20:02:11 +0100

    New Logo and Web Design

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 
+let SubheadNavs = [
+    document.getElementById("navAbout"),
+    document.getElementById("navPricing"),
+    document.getElementById("navContact")
+]
+
+let Subheaders = [
+    document.getElementById("About"),
+    document.getElementById("Pricing"),
+    document.getElementById("Contact")
+];
+
+function scrollUpdate(){
+    
+}
