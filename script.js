20ef9c9517ed2b1cf14bd398262ecdec32d54748
Dorcat <jaysharp.lee@gmail.com>
2026-06-25 10:20:51 +0100

    About Section First Pass + Mobile Adjustments

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 
+
+
 function scrollUpdate(){
+    const Hidden = document.querySelector('.header');
+    const Buffer = document.querySelector('.headerBuffer');
     
+    var Rect = Buffer.getBoundingClientRect();
+    var Mid = Rect.bottom;//(Rect.top + Rect.bottom) * 0.5;
+    if(Mid <= 50){
+        Hidden.className = 'header hidden';
+    } else {
+        Hidden.className = 'header';
+    }
 }
+
+onscroll = scrollUpdate;
